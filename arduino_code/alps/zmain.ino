4937b33a7e7dca2ca4c05ca97a256cab8ae917d0
Lorenzo Bossi <lorenzobossi83@gmail.com>
2020-06-20 22:54:27 +0100

    Added copyright notice and license on each file.

--- arduino_code/alps/zmain.ino
+++ arduino_code/alps/zmain.ino
@@ -1,3 +1,23 @@
+/**
+ *  Copyright 2020 Lorenzo Bossi
+ *
+ *  This file is part of ALPS (Another Light Painting Stick).
+ *
+ *  ALPS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ALPS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ALPS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 
 LedControl ledControl(NUMPIXELS, LED_STRIP_PIN);
 Protocol protocol(&ledControl, cmdSerial, dbgSerial);
